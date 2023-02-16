b993a284a5347c3d4f4fe2fbb5afae616dfc5890
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2023-02-16 15:01:45 -0600

    KJV Bot

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# King James Bible Bot
- A Discord bot that allows you to send Bible verses.
+<div align="center">
+    <img src="./README/icon.png" width="100"/>
+    <h1>King James Bible Bot</h1>
+</div>
+The King James Bible Bot is a Discord bot that provides users with quick access to the KJV Bible. By simply typing the reference to a verse, users will receive the corresponding text from the Bible. This bot is an incredibly convenient tool for those who regularly study the Bible or those who want to easily access a particular verse. The King James Bible Bot's ability to quickly send verses in a clear and concise manner makes it an excellent resource for both personal use and for religious study groups or communities. The bot's integration with Discord, a popular communication platform, makes it a great choice for those who want to incorporate their faith into their online social interactions.
---
+++ main.py
@@ -0,0 +1,73 @@
+import discord, requests, json, re
+
+def Find_Bible_References(text):
+    books = {"Genesis": ["gen", "ge", "gn"], "Exodus": ["exo", "ex"], "Leviticus": ["lev", "le"],
+             "Numbers": ["num", "nu"], "Deuteronomy": ["deut", "dt"], "Joshua": ["josh", "jsh"], "Judges": ["judg", "jdg"],
+             "Ruth": ["rut", "rt"], "1 Samuel": ["1sam", "1 sm", "1samuel"], "2 Samuel": ["2sam", "2 sm", "2samuel"],
+             "1 Kings": ["1kgs", "1 ki", "1kings"], "2 Kings": ["2kgs", "2 ki", "2kings"], "1 Chronicles": ["1chr", "1 ch", "1chronicles"],
+             "2 Chronicles": ["2chr", "2 ch", "2chronicles"], "Ezra": ["ezr"], "Nehemiah": ["neh", "ne"],
+             "Esther": ["est", "et"], "Job": ["job", "jb"], "Psalms": ["ps", "psa"], "Proverbs": ["prov", "pro"],
+             "Ecclesiastes": ["eccles", "ecc"], "Song of Solomon": ["sos"], "Isaiah": ["isa", "is"],
+             "Jeremiah": ["jer", "je"], "Lamentations": ["lam", "la"], "Ezekiel": ["ezek", "ezk"], "Daniel": ["dan", "dn"],
+             "Hosea": ["hos", "ho"], "Joel": ["joel", "jl"], "Amos": ["amos", "am"], "Obadiah": ["obad", "ob"],
+             "Jonah": ["jonah", "jn"], "Micah": ["micah", "mic"], "Nahum": ["nah", "na"], "Habakkuk": ["hab", "hk"],
+             "Zephaniah": ["zeph", "zp"], "Haggai": ["hag", "hg"], "Zechariah": ["zech", "zc"], "Malachi": ["mal", "ml"],
+             "Matthew": ["mt", "matt"], "Mark": ["mk", "mar"], "Luke": ["lk", "luk"], "John": ["jn", "jhn"], "Acts": ["acts"],
+             "Romans": ["rom"], "1 Corinthians": ["1cor", "1 corinthians"], "2 Corinthians": ["2cor", "2 corinthians"],
+             "Galatians": ["gal"], "Ephesians": ["eph"], "Philippians": ["php"], "Colossians": ["col"],
+             "1 Thessalonians": ["1thess", "1 thessalonians"], "2 Thessalonians": ["2thess", "2 thessalonians"],
+             "1 Timothy": ["1tim", "1 timothy"], "2 Timothy": ["2tim", "2 timothy"], "Titus": ["titus"],
+             "Philemon": ["phlm", "philem"], "Hebrews": ["heb"], "James": ["jas", "jam"], "1 Peter": ["1pet", "1 peter"],
+             "2 Peter": ["2pet", "2 peter"], "1 John": ["1jn", "1 john"], "2 John": ["2jn", "2 john"],
+             "3 John": ["3jn", "3 john"], "Jude": ["jude"], "Revelation": ["rev"]}
+
+    pattern = r"("
+    pattern += "|".join(books.keys())
+    pattern += r"|"
+    pattern += "|".join([abbr for abbrs in books.values() for abbr in abbrs])
+    pattern += r")\s+(\d+)(?::(\d+))?(?:-(\d+))?"
+
+    regex = re.compile(pattern, re.IGNORECASE)
+    matches = regex.findall(text)
+    references = []
+    for match in matches:
+        full_book_name = next((book for book, abbreviations in books.items() if match[0].lower() in abbreviations), match[0])
+        references.append((full_book_name, int(match[1]), int(match[2]) if match[2] else None, int(match[3]) if match[3] else None))
+
+    return references
+
+def Get_Passage(book, chapter, start_verse, end_verse):
+    url = "https://bible-api.com/"
+    if end_verse == 0:
+        url += book+"%20"+str(chapter)+":"+str(start_verse)
+    else:
+        url += book+"%20"+str(chapter)+":"+str(start_verse)+"-"+str(end_verse)
+    url += "?translation=kjv"
+    JSON = requests.get(url)
+    JSON = json.loads(JSON.text)
+    return JSON
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}!')
+        return await client.change_presence(activity=discord.Game(name='Something'))
+    
+    async def on_message(self, message):
+        BibleJson = []
+        BibleVerses = Find_Bible_References(message.content)
+        for verse in BibleVerses:
+            if(verse[3] == None):
+                BibleJson.append(Get_Passage(verse[0], verse[1], verse[2], 0))
+            else:
+                BibleJson.append(Get_Passage(verse[0], verse[1], verse[2], verse[3]))
+        for Json in BibleJson:
+            desc = ""
+            for v in Json["verses"]:
+                desc += "<**"+str(v["verse"])+"**> "+v["text"].strip()+" "
+            embed = discord.Embed(title=":closed_book: ** "+Json["reference"]+" **", description=desc, color=10450525)
+            await message.channel.send(embed=embed)
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = Client(intents=intents, description="This bot will recite any verse from the KJV", command_prefix="/", pm_help = True)
+client.run('MTA3NTU0MzgzMzk0MjY5NTk4Ng.GJ8m33.tuSbIy_N13k0AvLQ_Gap04nQzE4Km5zcp2nuQ4')
